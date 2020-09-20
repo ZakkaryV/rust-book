2d8a850d7174c6cdd871a67801754b450c36854c
Zakkary Verrilli <zverrilli@protonmail.com>
2020-09-20 12:48:49 -0700

    ch-12; eprintln! to pipe stderr

--- src/bin/ch_12_minigrep.rs
+++ src/bin/ch_12_minigrep.rs
@@ -5,26 +5,15 @@ use lib::minigrep;
 use std::env;
 
 fn main() {
-    // invoking the collect implemented specifically for T: Vec<String>
     let args: Vec<String> = env::args().collect();
+
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("Error: {}", err);
+        eprintln!("Error: {}", err);
         std::process::exit(1);
     });
 
-    println!(
-        "\nSearching for: {:?}\nIn File: {:?}\n",
-        config.query, config.filename
-    );
-
-    // read_to_string() returns a Result<String, std::error::Error>
-
-    // ownership is not taken by println! even though we are passing a non-reference
-
-    // ownership is taken of config.filename here
-
     if let Err(e) = minigrep::run(config) {
-        println!("App error: {}", e);
+        eprintln!("App error: {}", e);
         std::process::exit(1);
     }
 }
